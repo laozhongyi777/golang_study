89e1d27af6710277b87a09d4e242362466f45a31
laozhongyi777 <qjkk88369yy@gmail.com>
2019-02-23 00:11:18 +0400

    开学第5天

---
+++ struct/struct-literals.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2} 
+	v2 = Vertex{X: 1}　　
+	v3 = Vertex{ }	
+	p  = &Vertex{1, 2}  
+
+)
+
+func main() {
+	fmt.Println(v1, p, v2, v3)
+}
+//v1 = Vertex{1, 2} 
+//创建一个　Ｖertex 类型结构体
+//v2 = Vertex{X: 1}　　
+//Ｙ:0  被隐式地赋予
+//v3 = Vertex{}	
+//  X:0  Y:0
+//p  = &Vertex{1, 2}  
+//创建一个　*Vertex 类型的结构体（指针）
