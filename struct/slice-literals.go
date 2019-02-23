452ed044ff519a60b15c417bfa424abb48773f90
laozhongyi777 <qjkk88369yy@gmail.com>
2019-02-23 16:51:10 +0400

    开学第6天

--- struct/slice-literals.go
+++ struct/slice-literals.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	q := []int{2, 3, 5, 7, 11, 13}
+	q := []int{2, 3, 5, 7, 11, 13}  //数据不写表示全部
 	fmt.Println(q)
 
 	r := []bool{true, false, true, true, false, true}
